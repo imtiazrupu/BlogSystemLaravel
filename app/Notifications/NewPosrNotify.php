4dd43e618952248e5e0bcddbdb794a90679cd28a
Imtiaz Rupu <imtiazrupucse2@gmail.com>
2019-07-17 16:51:56 +0600

    Queueing Notifications

--- app/Notifications/NewPosrNotify.php
+++ app/Notifications/NewPosrNotify.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class NewPosrNotify extends Notification
+class NewPosrNotify extends Notification implements ShouldQueue
 {
     use Queueable;
     public $post;
