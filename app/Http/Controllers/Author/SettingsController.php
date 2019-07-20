8c7ac190953a0503fdb0b8f708b080670675b75c
Imtiaz Rupu <imtiazrupucse2@gmail.com>
2019-07-20 15:52:26 +0600

    Author Settings Profile Update and Change Password

---
+++ app/Http/Controllers/Author/SettingsController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Author;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+use Brian2694\Toastr\Facades\Toastr;
+use Illuminate\Support\Facades\Hash;
+
+class SettingsController extends Controller
+{
+    public function index()
+    {
+        return view('author.settings');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required',
+            'email' => 'required|email',
+            'image' => 'image',
+        ]);
+        // get form image
+        $image = $request->file('image');
+        $slug = str_slug($request->name);
+        $user = User::findOrFail(Auth::id());
+        if(isset($image))
+        {
+            // make unique name for image
+            $currentDate = Carbon::now()->toDateString();
+            $imageName = $slug.'-'.$currentDate.'-'.uniqid().'.'.$image->getClientOriginalExtension();
+            // check post directory is exists
+            if(!Storage::disk('public')->exists('profile'))
+            {
+                Storage::disk('public')->makeDirectory('profile');
+            }
+            //delete old image
+            if(Storage::disk('public')->exists('profile/'.$user->image))
+            {
+                Storage::disk('public')->delete('profile/'.$user->image);
+            }
+            // resize image for profile and upload
+            $profileImage = Image::make($image)->resize(500,500)->stream();
+            Storage::disk('public')->put('profile/'.$imageName,$profileImage);
+          }else{
+            $imageName = $user->image;
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->image = $imageName;
+        $user->about = $request->about;
+        $user->save();
+        Toastr::success('Profile Updated Successfully', 'Success');
+        return redirect()->back();
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $this->validate($request,[
+            'old_password' => 'required',
+            'password' => 'required|confirmed',
+        ]);
+
+        $hashedPassword = Auth::user()->password;
+        if(Hash::check($request->old_password, $hashedPassword))
+        {
+            if(!Hash::check($request->password, $hashedPassword))
+            {
+                $user = User::find(Auth::id());
+                $user->password = Hash::make($request->password);
+                $user->save();
+                Toastr::success('Password Updated Successfully', 'Success');
+                Auth::logout();
+                return redirect()->back();
+            }else{
+                Toastr::error('New password can not be same as old password', 'Error');
+                return redirect()->back();
+            }
+        }else{
+            Toastr::error('Cuurent password not match Successfully', 'error');
+            return redirect()->back();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Auth::routes();
 Route::group(['as'=>'admin.','prefix' => 'admin', 'namespace' => 'Admin','middleware'=>['auth','admin']], function () {
     Route::get('dashboard','DashboardController@index')->name('dashboard');
 
+    //Admin Profile Update
     Route::get('settings','SettingsController@index')->name('settings');
     Route::put('profile/update','SettingsController@updateProfile')->name('profile.update');
     Route::put('password/update','SettingsController@updatePassword')->name('password.update');
@@ -38,5 +39,11 @@ Route::group(['as'=>'admin.','prefix' => 'admin', 'namespace' => 'Admin','middle
 
 Route::group(['as'=>'author.','prefix' => 'author', 'namespace' => 'Author','middleware'=>['auth','author']], function () {
     Route::get('dashboard','DashboardController@index')->name('dashboard');
+
+     //Author Profile Update
+     Route::get('settings','SettingsController@index')->name('settings');
+     Route::put('profile/update','SettingsController@updateProfile')->name('profile.update');
+     Route::put('password/update','SettingsController@updatePassword')->name('password.update');
+
     Route::resource('post','PostController');
 });
