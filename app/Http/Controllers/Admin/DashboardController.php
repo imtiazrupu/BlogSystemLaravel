f88347bf830e0f9c167440afb6071ba6e8f866e1
Imtiaz Rupu <imtiazrupucse2@gmail.com>
2019-07-31 20:17:42 +0600

    Admin Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,39 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Post;
+use App\Category;
+use App\Tag;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('admin.dashboard');
+        $posts = Post::all();
+        $populer_posts = Post::withCount('comments')
+                                    ->withCount('favorite_to_users')
+                                    ->orderBy('view_count','desc')
+                                    ->orderBy('comments_count')
+                                    ->orderBy('favorite_to_users_count')
+                                    ->take(5)->get();
+        $total_pending_posts = Post::where('is_approved',false)->count();
+        $all_views = Post::sum('view_count');
+        $author_count = User::where('role_id',2)->count();
+        $new_authors_today = User::where('role_id',2)
+                                    ->whereDate('created_at',Carbon::today())->count();
+        $active_authors = User::where('role_id',2)
+                                    ->withCount('posts')
+                                    ->withCount('comments')
+                                    ->withCount('favorite_posts')
+                                    ->orderBy('posts_count','desc')
+                                    ->orderBy('comments_count')
+                                    ->orderBy('favorite_posts_count','desc')
+                                    ->take(10)->get();
+        $category_count = Category::all()->count();
+        $tag_count = Tag::all()->count();
+        return view('admin.dashboard',compact('posts','populer_posts','total_pending_posts','all_views',
+                                     'author_count','new_authors_today','active_authors','category_count','tag_count'));
     }
 }
